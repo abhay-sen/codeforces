194259c843c1c62f938f00a353f7b6205618e731
abhay-sen <abhaysen0502@gmail.com>
2023-11-14 15:16:08 +0530

    it was wrong

--- A_Watermelon.cpp
+++ A_Watermelon.cpp
@@ -3,14 +3,19 @@ using namespace std;
 int main() {
     int n;
     cin>> n;
-    if (n%2==0)
+    if (n==2)
     {
-        cout<< "YES";
+        cout<< "NO";
+    }
+    else if(n%2==0)
+    {
+        cout<<"YES";
     }
     else
     {
         cout<<"NO";
     }
+    
     
     
 
