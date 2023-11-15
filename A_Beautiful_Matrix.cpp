2ce8923b55e9f441ad88ecaf9044dc05bd06280f
abhay-sen <abhaysen0502@gmail.com>
2023-11-15 14:03:05 +0530

    15/11

---
+++ A_Beautiful_Matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int matrix;
+    int row,col;
+    for (int i = 1; i <=5 ; ++i){
+        for (int j = 1; j <= 5; j++)
+        {
+            cin>>matrix;
+            if (matrix==1)
+            {
+                row=i;
+                col=j;
+            }
+            
+        }
+        
+
+    }
+    cout<< abs(row-3) + abs(col-3);
+}
